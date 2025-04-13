6eae42b7b868ba8800e329177bfe8b8eafa08cf4
Anukalp Raj <122322905+anukalpr@users.noreply.github.com>
2025-04-13 20:07:38 +0530

    Add files via upload

---
+++ Client/src/myApp.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Header from './Components/Header';
+import SideBar from './Components/SideBar';
+import {useState} from 'react';
+import Emails from './Components/Emails';
+function myApp() {
+  const [openDrawer, setOpenDrawer]=useState(true);
+  const toggleDrawer=()=>{
+    setOpenDrawer(prevState=>!prevState);
+  }
+  return (
+    <div>
+      <Header toggleDrawer={toggleDrawer}/>
+      <SideBar openDrawer={openDrawer}/>
+      <Emails openDrawer={openDrawer}/>
+    </div>
+  )
+}
+
+export default myApp;
